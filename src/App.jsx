d31906ad8ac8b65c989dc0936d1db807c3e876fd
Emmanuel <zacmac807@gmail.com>
2024-08-31 01:12:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Liquor store management system</h1>
+        <h1>M-Squared</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
